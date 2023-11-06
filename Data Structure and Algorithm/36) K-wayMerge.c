56e45d49749d4d33f6b41ee5641f50cdc4e70c35
NISHANT KUMAR <144478754+nishant-kumarr@users.noreply.github.com>
2023-11-06 16:08:07 +0530

    Updated K-way merge

--- Data Structure and Algorithm/36) K-wayMerge.c
+++ Data Structure and Algorithm/36) K-wayMerge.c
@@ -1,3 +1,11 @@
+/*
+
+Hey Visitor ! This is my C program that performs K-way merge. It takes 'n' no of sorted or unsorted arrays
+along with the their size from the user and return a final array - merging all the arrays in sorted (ascending) order.
+Check out the overall concept of program at the bottom.
+
+*/
+
 #include <stdio.h>
 #include<stdlib.h>
 
@@ -63,38 +71,65 @@ void mergearrays(int* arr[], int k, int size[]){
     for(int i=0;i<c;i++){
         printf("%d ",a[i]);
     }
+    printf("\n.");
 }
 
 int main(){
     int n,i,k,j=0;
+
     do{
-        printf("\nEnter the number of the arrays:");
+        printf("\nEnter the number of the arrays: ");
         scanf("%d",&k);
         
         if(k<=2)
             printf("\nplease enter more than 2 arrays.....\n");
-        else break;
+        else 
+            break;
     }while(1);
+
     int* arr[k];
     int size[k];
     for(i=0;i<k;i++){
-        printf("\nEnter the size of the %d array :",i+1);
+        printf("\nEnter the size of the %d array : ",i+1);
         scanf("%d",&size[i]);
         
         arr[i] = (int *)malloc(sizeof(int)*size[i]);
         
         for(j=0;j<size[i];j++){
-            printf("Enter the %d value:",j+1);
+            printf("Enter the %d value: ",j+1);
             scanf("%d",&arr[i][j]);
         }
     }
     
+    printf("\n-------------------------------------------\n\t\tResult : \n");
     for(i=0;i<k;i++){
-        printf("\nThe %d array :",i+1);
+        printf("\nArray %d data : ",i+1);
         for(j=0;j<size[i];j++){
             printf("%d ",arr[i][j]);
         }
     }
+    printf("\n");
     mergearrays(arr, k, size);
     return 0;
 }
+
+/* 
+
+PROGRAM FLOW AND DESIGN : 
+
+1. First we take value of 'n' that is no of array that need to be merged.
+2. Then, we take one by one their size and array data.
+3. We keep size details of each array in separate array named 'size'.
+4. Here in this program we have allocated memory dynamically because we can't create array
+    in static manner as no of array to be merged is not fixed, and it depends on user.
+    so, some time no of array can be 3, other time it can be 7 etc.
+5. We have also taken care if passed array are not sorted then we sort them first.
+6. Three functions used in program apart from main are : merge, mergesort and mergearrays.
+7. 'mergesort' is the function that sort the passed arrays if any of them is not sorted.
+8. 'mergesort' take help of 'merge' function to merge them as we are using Merge sort algorithm.
+9. and the last 'mergearray' merge all the sorted arrays.
+10. One more thing how we are dealing with size is we store first array size at first index in 'size' array
+    size of second at seconde index and so on.
+11. Hope overall idea is clear for logic understand prefer reading code.
+
+*/
